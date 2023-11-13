a7c77ed2f344d2e1fd5843a52d2e1a98663823d2
diego <diegozaratean@gmail.com>
2023-11-13 17:21:48 +0000

    google geocoding integration

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,14 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import googlemaps
+from datetime import datetime
+import os
+
+gmaps = googlemaps.Client(key=os.getenv("GOOGLEAPI"))
+
+
+
 
 api = Blueprint('api', __name__)
 
@@ -14,9 +22,12 @@ CORS(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
+    # Geocoding an address
+    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request",
+        'geocode_result':geocode_result
     }
 
     return jsonify(response_body), 200
